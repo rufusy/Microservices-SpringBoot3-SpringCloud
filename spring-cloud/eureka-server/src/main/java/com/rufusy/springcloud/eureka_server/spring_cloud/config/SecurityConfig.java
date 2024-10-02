c10f17c6c66fbbfbb65c24ed3952f7d25d95db3f
rufusy <idachirufus@gmail.com>
2024-10-03 00:51:01 +0300

    secure eureka with http basic auth

---
+++ spring-cloud/eureka-server/src/main/java/com/rufusy/springcloud/eureka_server/spring_cloud/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.rufusy.springcloud.eureka_server.spring_cloud.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    private final String username;
+    private final String password;
+
+    @Autowired
+    public SecurityConfig(
+            @Value("${app.eureka-username}") String username,
+            @Value("${app.eureka-password}") String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_USER"));
+
+        UserDetails user = new User(username, passwordEncoder().encode(password), authorities);
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.anyRequest().authenticated())
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+}
