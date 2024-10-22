f714f51bf43773ccd06b7e4d82248bd27e5a7afb
rufusy <idachirufus@gmail.com>
2024-10-22 19:42:04 +0300

    add workarounds for supporting non-reactive clients

--- microservices/product-service/src/main/java/com/rufusy/microservices/core/product/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/com/rufusy/microservices/core/product/ProductServiceApplication.java
@@ -5,14 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import reactor.core.publisher.Hooks;
 
 @Slf4j
 @SpringBootApplication
 @ComponentScan("com.rufusy")
 public class ProductServiceApplication {
 	public static void main(String[] args) {
+		Hooks.enableAutomaticContextPropagation();
 		ConfigurableApplicationContext ctx = SpringApplication.run(ProductServiceApplication.class, args);
-
 		String mongodbHost = ctx.getEnvironment().getProperty("spring.data.mongodb.host");
 		String mongodbPort =  ctx.getEnvironment().getProperty("spring.data.mongodb.port");
 		log.info("Connected to MongoDb: {} : {}", mongodbHost, mongodbPort);
